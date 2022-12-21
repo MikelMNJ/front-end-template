9b17264c6fd8b92e69a0c5dd7ed85753227c585f
Mikel Jagan <mikelmnj@gmail.com>
2022-12-21 07:31:48 -0500

    Added `Copyright` component from `xerum` + app version number to footer.

--- src/scenes/App/Footer.js
+++ src/scenes/App/Footer.js
@@ -1,5 +1,9 @@
 import { StyledFooter } from './styles';
 import { Font } from 'components';
+import { Copyright } from 'xerum';
+import * as PackageJSON from '../../../package.json';
+
+const { version } = PackageJSON;
 
 const Footer = props => {
   const { theme, selectedTheme } = props;
@@ -7,7 +11,16 @@ const Footer = props => {
   return (
     <StyledFooter theme={theme} selectedTheme={selectedTheme}>
       <Font size={0.9}>
-        <p>©2021, Company Name &mdash; Made with love</p>
+        <Copyright
+          trade={true}
+          rights={true}
+          name='[Company]'
+          message='made for you.'
+        />
+      </Font>
+
+      <Font weight='light' size={0.9}>
+        v{version}
       </Font>
     </StyledFooter>
   );
