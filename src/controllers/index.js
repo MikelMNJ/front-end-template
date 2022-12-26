3328485f492766fef6bf348f1013e142211176eb
Mikel Jagan <mikelmnj@gmail.com>
2022-12-26 06:46:28 -0500

    Extracted `fonts` to its own controller.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,3 @@
+export * from './fontsController';
 export * from './routesController';
 export * from './reducersController';
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,15 +1,5 @@
 import { appConstants } from 'modules';
-import {
-  NunitoExtraLight,
-  NunitoLight,
-  NunitoRegular,
-  NunitoBold,
-  NunitoBlack,
-  OpenSansLight,
-  OpenSansRegular,
-  OpenSansBold,
-  OpenSansExtraBold,
-} from 'static';
+import { fonts } from 'controllers';
 
 const { themes } = appConstants;
 
@@ -77,86 +67,6 @@ const modes = {
     warning: colors.warning,
     error: colors.congoPink,
     info: colors.info,
-  },
-};
-
-const fonts = {
-  primary: {
-    thin: {},
-    extraLight: {
-      family: 'Nunito-ExtraLight',
-      src: NunitoExtraLight.default,
-      format: 'truetype',
-      weight: 200,
-      style: 'normal',
-    },
-    light: {
-      family: 'Nunito-Light',
-      src: NunitoLight.default,
-      format: 'truetype',
-      weight: 300,
-      style: 'normal',
-    },
-    normal: {
-      family: 'Nunito-Regular',
-      src: NunitoRegular.default,
-      format: 'truetype',
-      weight: 400,
-      style: 'normal',
-    },
-    medium: {},
-    semiBold: {},
-    bold: {
-      family: 'Nunito-Bold',
-      src: NunitoBold.default,
-      format: 'truetype',
-      weight: 700,
-      style: 'normal',
-    },
-    extraBold: {},
-    black: {
-      family: 'Nunito-Black',
-      src: NunitoBlack.default,
-      format: 'truetype',
-      weight: 900,
-      style: 'normal',
-    },
-  },
-
-  secondary: {
-    thin: {},
-    extraLight: {},
-    light: {
-      family: 'OpenSans-Light',
-      src: OpenSansLight.default,
-      format: 'truetype',
-      weight: 300,
-      style: 'normal',
-    },
-    normal: {
-      family: 'OpenSans-Regular',
-      src: OpenSansRegular.default,
-      format: 'truetype',
-      weight: 400,
-      style: 'normal',
-    },
-    medium: {},
-    semiBold: {},
-    bold: {
-      family: 'OpenSans-Bold',
-      src: OpenSansBold.default,
-      format: 'truetype',
-      weight: 700,
-      style: 'normal',
-    },
-    extraBold: {
-      family: 'OpenSans-ExtraBold',
-      src: OpenSansExtraBold.default,
-      format: 'truetype',
-      weight: 800,
-      style: 'normal',
-    },
-    black: {},
   },
 };
 
